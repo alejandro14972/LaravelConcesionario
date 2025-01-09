03443f60dd90f3392b0283202dda9ac14dc2a30d
Alejandro <pfu09125@educastur.es>
2025-01-09 19:01:35 +0100

    el usuario es redireccioando y se muestra un mensaje

--- resources/views/vehiculos/index.blade.php
+++ resources/views/vehiculos/index.blade.php
@@ -7,6 +7,18 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+
+            @if (@session()->has('mensaje'))
+                
+            <div class="bg-green-500 p-2 text-white rounded mb-4">
+                {{ session('mensaje') }}
+            </div>
+          
+                
+            @endif
+
+
+
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
                     {{ __("Mis vehiculos") }}

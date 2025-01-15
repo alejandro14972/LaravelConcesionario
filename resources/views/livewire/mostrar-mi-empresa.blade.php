d001ef317b004ba087303d036b948fc8e3728eb8
Alejandro <pfu09125@educastur.es>
2025-01-15 16:57:15 +0100

    se añadio la funcionalidad de eliminar empresa

--- resources/views/livewire/mostrar-mi-empresa.blade.php
+++ resources/views/livewire/mostrar-mi-empresa.blade.php
@@ -27,7 +27,7 @@
 
 
                 <div class="flex gap-3 mt-5 justify-center md:flex md:justify-center md:items-center">
-                    <a href="#" class="bg-slate-800 py-2 px-4 text-white rounded hover:bg-slate-900 uppercase">
+                    <a href="#" class="bg-green-800 py-2 px-4 text-white rounded hover:bg-slate-900 uppercase">
                         Ver
                     </a>
 
@@ -36,9 +36,11 @@
                         Editar
                     </a>
 
-                    <a href="#" class="bg-red-800 py-2 px-4 text-white rounded hover:bg-slate-900 uppercase">
+                    <button 
+                        wire:click="$dispatch('mostrarAlerta', { id: {{ $empr->id }} })" 
+                        class="bg-red-800 py-2 px-4 text-white rounded hover:bg-slate-900 uppercase">
                         Eliminar
-                    </a>
+                    </button>
                 </div>
 
             </div>
@@ -47,3 +49,33 @@
     </div>
 
 </div>
+
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        document.addEventListener('livewire:initialized', () => {
+            Livewire.on('mostrarAlerta', (empresaId) => {
+                Swal.fire({
+                    title: '¿Eliminar empresa?',
+                    text: "Un vez eliminada no se puede recuperar.",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Sí, eliminar',
+                    cancelButtonText: 'Cancelar'
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        @this.call('eliminarEmpresa', empresaId);
+                        Swal.fire(
+                            'Eliminada!',
+                            'La empresa fue eliminadoa correctamente.',
+                            'success'
+                        );
+                    }
+                });
+            });
+        });
+    </script>
+@endpush
